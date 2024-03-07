d2f3679aa0bbf34ac2f1e751bdaf29fd8326b175
11notes <github@11notes.ch>
2024-03-07 12:03:35 +0100

    better verbose

--- rootfs/certbot/scripts/vmware.horizon.uag.sh
+++ rootfs/certbot/scripts/vmware.horizon.uag.sh
@@ -10,13 +10,13 @@
   #
   trap "rm -f ${ROOT}/${1}.json &> /dev/null" EXIT  
   ROOT=${APP_ROOT}/var/${1}
-  SCRIPT=${PWD}/$(basename "$0")
+  SCRIPT=${PWD}/$(basename "${0}")
 
   openssl x509 -noout -text -in ${ROOT}/${1}.crt | grep -q "ecPublicKey"
   if [ $? == 1 ]; then
     END=$(echo -n Q | openssl s_client -connect ${HORIZON_VIEW_FQDN}:443 2>/dev/null | openssl x509 -enddate -noout -checkend 3)
     if echo ${END} | grep -q 'will expire'; then
-      elevenLogJSON info "script \"${SCRIPT}\" certificate will expire in the next three days, replacing ..."
+      elevenLogJSON info "[${SCRIPT}] certificate will expire in the next three days, replacing ..."
       echo '{}' | jq \
         --arg privateKeyPem "$(cat ${ROOT}/${1}.key)" \
         --arg certChainPem "$(cat ${ROOT}/${1}.crt)" \
@@ -25,14 +25,14 @@
       for IP in ${HORIZON_VIEW_UAG_NODES}; do
         curl -i -s -o /dev/null -w "%{http_code}" -X PUT --insecure --max-time 5 --user ${HORIZON_VIEW_UAG_USER}:${HORIZON_VIEW_UAG_PASSWORD} -H 'Content-Type: application/json' https://${IP}:9443/rest/v1/config/certs/ssl -d @${ROOT}/${1}.json | grep -q '200'
         if [ ! $? == 0 ]; then
-          elevenLogJSON error "script \"${SCRIPT}\" failed to update Unified Access Gateway [${IP}]!"
+          elevenLogJSON error "[${SCRIPT}] failed to update Unified Access Gateway [${IP}]!"
         else
-          elevenLogJSON info "script \"${SCRIPT}\" certificate upadted on Unified Access Gateway [${IP}]"
+          elevenLogJSON info "[${SCRIPT}] certificate upadted on Unified Access Gateway [${IP}]"
         fi
       done
     else
-      elevenLogJSON info "script \"${SCRIPT}\" no updated of certificate needed, does not expire in the next three days"
+      elevenLogJSON info "[${SCRIPT}] no updated of certificate needed, does not expire in the next three days"
     fi
   else
-    elevenLogJSON error "script \"${SCRIPT}\" can't be used on ecdsa certificates, make sure you have selected key:rsa for Horizon View Unified Access Gateway!"
+    elevenLogJSON error "[${SCRIPT}] can't be used on ecdsa certificates, make sure you have selected key:rsa for Horizon View Unified Access Gateway!"
   fi
